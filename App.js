4ef1b6d80962d8d0a2914e28bfdca09fabe9a257
Olivier Payen <olivier.payen@klarna.com>
2022-06-08 11:47:19 +0200

    Show timestamps in UI

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -60,36 +60,29 @@ const App: () => Node = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [timestamps, setTimestamps] = useState([]);
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <Button
-          onPress={() => console.log(global.performance.now())}
+          onPress={() =>
+            setTimestamps(arr => [...arr, global.performance.now()])
+          }
           title="performance.now()"
         />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        <Text>
+          Diff between last two:{' '}
+          {timestamps[timestamps.length - 1] -
+            timestamps[timestamps.length - 2]}
+        </Text>
+
+        {timestamps.map(timestamp => (
+          <Text key={timestamp}>{timestamp}</Text>
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
